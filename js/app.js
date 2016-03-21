e58ffd5fff85360c59a4d18fd7e7341da686d023
andreaainara <andrea.ainara10@gmail.com>
2016-03-21 09:12:12 -0700

    ALL OF THE THINGS

--- js/app.js
+++ js/app.js
@@ -1,61 +1,33 @@
-var turn = "X";
-var moves = 0;
-
-
-function resetGame() {}
- $(document).ready(function() {
-   turn = "X";
-   moves = 0;
- }
-
-
+// wait for the DOM to finish loading
+var turn = 0;
 
 $(document).ready(function() {
-   return diagonalWinner() || (rowWinner() || columnWinner());
- });
+  // all code to manipulate the DOM
+  // goes inside this function
 
-
-   function boardHasEmptyBoxes() {
-    var hasEmptyBoxes = false;
-    for (var i=0; i<$boxes.length; i++){
-      if ($boxes.eq(i).text() === ''){
-        hasEmptyBoxes = true;
+  $(".box").on("click", function onClick(event) {
+    if($(this).html() === '') {
+      if (turn % 2 !==0) {
+        $(this).text('O');
+        turn +=1;
+      } else {
+        $(this).text('X');
+        turn +=1;
       }
+    } else {
+        return false;
     }
-    return hasEmptyBoxes;
-  }
+
+
+  });
+$("button.btn").on("click", function handleReset(event) {
+  $(".box").text('');
+  turn = 0;
+});
 
 
 
 
-$(document).ready(function() {
- });
 
 
- $boxes.on('click', function() {
-   if ($(this).text() === "") {
-
- $(document).ready(function() {
-     $(this).text(turn);
-     $(this).addClass(turn);
-     moves += 1;
-   }
- );
- }
-
-
-     var winner = getWinner();
-
- $(document).ready(function() {
-       alert("Player " + winner + " won!");
-       resetGame();
-    else if (moves < 9); {
-    else if (boardHasEmptyBoxes()) {
-       changeTurn();
-    else {
-      alert("Neither player won!");
-      resetGame();
-    }
-  }
-}
 });
